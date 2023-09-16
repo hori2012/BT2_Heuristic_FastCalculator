8cb8b69fc699c508dc3e3c0d599dbd9902a499b0
Cong Hao <conghao.hc@gmail.com>
2023-09-16 11:49:07 +0700

    Add project files.

---
+++ BT2_Heuristic_FastCalculator/BT2_Heuristic_FastCalculator.py
@@ -0,0 +1,152 @@
+﻿#Kiem tra tinh hop le cua bieu thuc
+#Chi kiem tra bieu thuc chua so va toan tu
+#Khong cho hai toan tu lien nhau, vi du: 8 + -7
+#Chua kiem tra het cac truong hop khac: co dau ngoac,  8 + - 7 = 8 - 7,...
+from asyncio.windows_events import NULL
+
+
+def valid_expression(exprs):
+    operators = ['+', '-', '*']
+    for x in range(len(exprs)):
+        if exprs[x] in operators and exprs[x + 1] in operators:
+            return False
+        elif exprs[x].isdigit() or exprs[x] in operators:
+            continue
+        else: return False
+    return True   
+        
+#Kiem tra dang cua bieu thuc
+#Bieu thuc chi co +, - thi tra ve 0
+#Bieu thuc chi co * thi tra ve 1
+#Bieu thuc co +. -, * thi tra ve -1
+def check_expression(exprs):
+    if ('+' in exprs) or ('-' in exprs):
+        if '*' in exprs:
+            return -1
+        else: return 0
+    else: return 1
+
+#Quy tac 1: Cac so hang doi nhau
+def combine_terms_rul1(expr):
+    terms = []
+    term = ''
+    result = ''
+    for char in expr:
+        if char.isdigit() or (char == '-' and term == ''):
+            term += char
+        else:
+            terms.append(int(term))
+            term = char
+    terms.append(int(term))
+    i = 0
+    terms_copy = terms.copy()
+    for term in terms_copy:
+        if term in terms and -term in terms:
+            terms.remove(term)
+            terms.remove(-term)
+    for i, x in enumerate(terms):
+        if x < 0 or i == 0:
+            result += str(x)
+        else:
+            result = result + '+' + str(x)
+    return result
+
+#Quy tac 2: Gom nhóm các số hạng có tổng tròn trăm ,tròn chục, tròn nghìn.
+def combine_terms_rul2(expr):
+    terms = []
+    term = ''
+    for char in expr:
+        if char.isdigit() or (char == '-' and term == ''):
+            term += char
+        elif char == '-' and term != '':
+            terms.append(int(term))
+            term = char
+        else:
+            terms.append(int(term))
+            term = ''
+    terms.append(int(term))
+    terms_copy = terms.copy()
+    for i1, term1 in enumerate(terms_copy):
+        for i2, term2 in enumerate(terms_copy):
+            if i1 == i2:
+                 continue
+            elif (term1 + term2) % 10 == 0 and (term1 in terms and term2 in terms):
+                terms.remove(term1)
+                terms.remove(term2)
+                terms.append(term1 + term2)
+                break
+    result = ''
+    for i, x in enumerate(terms):
+        if x < 0 or i == 0:
+            result += str(x)
+        else:
+            result = result + '+' + str(x)
+    return result
+
+#Quy tac 3: Nhan voi 0
+def combine_terms_rul3(exprs):
+    for i in range(len(exprs)):
+        if exprs[i] == '*' and exprs[i+1] == '0':
+            return '0'
+    return exprs
+
+#Quy tac 4: Gom nhom cac so hang co tich tron tram, tron chuc, tron nghin
+def combine_terms_rul4(exprs):
+    terms = []
+    term =''
+    for char in exprs:
+        if char.isdigit():
+            term += char
+        else:
+            terms.append(int(term))
+            terms.append(char)
+            term = ''
+    terms.append(int(term))
+    terms_copy = terms.copy()
+    for i in range(0, len(terms_copy), 2):
+         for j in range(0, len(terms_copy), 2):
+             if i == j:
+                 continue
+             elif (terms_copy[i]*terms_copy[j]) % 10 == 0 and (terms_copy[i] in terms and terms_copy[j] in terms):
+                index1 = i
+                index2 = j
+                value1 = terms_copy[i]
+                value2 = terms_copy[j]
+                if i == 0:
+                    del terms[j],  terms[index2 - 1],  terms[i + 1],  terms[i]
+                    terms.append('*')
+                    terms.append(value1*value2)
+                    break
+                else:
+                    del terms[j],  terms[index2 - 1],  terms[i],  terms[index1 - 1]
+                    terms.append('*')
+                    terms.append(value1*value2)
+                    break
+    return ''.join(str(x) for x in terms)
+
+#Quy tac 5: Bo cac so 0 sau ket qua, va them lai cac so 0 vao ket qua cuoi cung
+def combine_terms_rul5(exprs):
+    
+    return NULL
+
+#Quy tac 7: Tinh tuan tu
+def combine_terms_rul7(exprs):
+    return eval(exprs)
+
+#Main 
+while True:
+    exprs = str(input("Enter the expression to calculate: "))
+    while not valid_expression(exprs):
+        exprs = str(input("Enter the expression to calculate: "))
+    exprs = exprs.replace(' ', '')
+    print("Result: ", end=' ')
+    if check_expression(exprs) == 0:
+        #Expression sample: 2+5-2+8-7-5+8+2-7+9
+        print(combine_terms_rul1(exprs))
+        print(combine_terms_rul2(exprs))
+        print(combine_terms_rul7(combine_terms_rul2(combine_terms_rul1(exprs))))
+    elif check_expression(exprs) == 1:
+       print(combine_terms_rul3(exprs))
+       print(combine_terms_rul4(exprs))
+    elif exprs == '':
+        break
